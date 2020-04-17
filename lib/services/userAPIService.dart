9503e95cad0e33717b2b7e92a44f81f0def2bb76
VINICIUS TAVANO FERREIRA <vinicius.tavano@riachuelo.com.br>
2020-04-17 08:54:45 -0300

    Implementação de inversão de controle/ injeção de dependências sob HttpClient.

--- lib/services/userAPIService.dart
+++ lib/services/userAPIService.dart
@@ -9,17 +9,21 @@ class UserAPIService implements APIService{
   String uri = "http://labs.believeit.com.br/projetos/tennis-app-api/api/user";
 
   @override
+  HttpClient httpClient;
+
+  UserAPIService({this.httpClient});
+
+  @override
   Future<String> post(Map body) async {
     // Em português: só faz sentido se as APIs pedirem trativas diferentes para cada recurso. Isso ainda não está definido.
     // TODO: set or maintain USER body accordingly.
-    var httpClient = new HttpClient();
-    final request = await HttpClient().postUrl(Uri.parse(this.uri));
+    final request = await this.httpClient.postUrl(Uri.parse(this.uri));
     request.headers.set('content-type', 'applicaton/json');
     request.add(utf8.encode(json.encode(body)));
     HttpClientResponse response = await request.close();
     // TODO: check response.statusCode.
     String reply = await response.transform(utf8.decoder).join();
-    httpClient.close();
+    this.httpClient.close();
     return reply;
 
   }
