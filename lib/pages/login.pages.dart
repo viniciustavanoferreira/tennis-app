264f4efce7390d7f142290d6cfd6d058d7f46d5a
VINICIUS TAVANO FERREIRA <vinicius.tavano@riachuelo.com.br>
2020-04-22 12:00:10 -0300

    Atualiza importação indevida da classe da página de login.

--- lib/pages/login.pages.dart
+++ lib/pages/login.pages.dart
@@ -1,4 +1,3 @@
-import 'package:controle_financeiro_pessoal/model/service.dart';
 import 'package:tennis_play_all/model/service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
